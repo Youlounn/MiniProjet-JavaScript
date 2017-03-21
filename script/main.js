afec425df211552d36aff9b6ed363ab2d9ca3d82
Youlounn <yohann.rialet28@gmail.com>
2017-03-21 20:57:28 +0100

    Massak commit

--- script/main.js
+++ script/main.js
@@ -1,9 +1,8 @@
 $(function(){
   var bool = 0;
   var nb = 20;
+
   $("#plus").click(function(){
-    $("#plus").css("transition","all 1s slide");
-    $("#reglages").css("transition","all 1s slide");
     if(bool==0){
       $("#reglages").css("display","block");
       bool = 1;
@@ -17,29 +16,11 @@ $(function(){
     nb = $("#reglages input").val();
     nb++;
     $("#reglages input").val(nb);
-    if(nb >= 100){
-      $("#reglages input").css("width","4%");
-    }
-    if(nb >= 1000){
-      $("#reglages input").css("width","5%");
-    }
-    if(nb >= 10000){
-      $("#reglages input").css("width","6%");
-    }
   })
   $("#decrementer").click(function(){
     nb = $("#reglages input").val();
     nb--;
     $("#reglages input").val(nb);
-    if(nb < 100){
-      $("#reglages input").css("width","3%");
-    }
-    if(nb < 1000){
-      $("#reglages input").css("width","4%");
-    }
-    if(nb < 10000){
-      $("#reglages input").css("width","5%");
-    }
   })
 
 });
