c55e10841336c1803d6418b6de05531d0a474e2d
Youlounn <yohann.rialet28@gmail.com>
2017-03-21 17:51:00 +0100

    Ajout modifications

--- script/main.js
+++ script/main.js
@@ -1,6 +1,9 @@
 $(function(){
   var bool = 0;
+  var nb = 20;
   $("#plus").click(function(){
+    $("#plus").css("transition","all 1s slide");
+    $("#reglages").css("transition","all 1s slide");
     if(bool==0){
       $("#reglages").css("display","block");
       bool = 1;
@@ -10,4 +13,33 @@ $(function(){
       bool = 0;
     }
   })
+  $("#incrementer").click(function(){
+    nb = $("#reglages input").val();
+    nb++;
+    $("#reglages input").val(nb);
+    if(nb >= 100){
+      $("#reglages input").css("width","4%");
+    }
+    if(nb >= 1000){
+      $("#reglages input").css("width","5%");
+    }
+    if(nb >= 10000){
+      $("#reglages input").css("width","6%");
+    }
+  })
+  $("#decrementer").click(function(){
+    nb = $("#reglages input").val();
+    nb--;
+    $("#reglages input").val(nb);
+    if(nb < 100){
+      $("#reglages input").css("width","3%");
+    }
+    if(nb < 1000){
+      $("#reglages input").css("width","4%");
+    }
+    if(nb < 10000){
+      $("#reglages input").css("width","5%");
+    }
+  })
+
 });
