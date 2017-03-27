6fcaf53ea4b99cbd9ae4bf990b6d35ac11627148
PonyHacker <Soudzy@gmail.com>
2017-03-27 10:47:02 +0200

    :smile:
    ça marche bien :dancer:

--- script/main.js
+++ script/main.js
@@ -23,6 +23,25 @@ $(function(){
     $("#reglages input").val(nb);
   })
 
+  var listeVille = [];
+
+  $.ajax({
+   url: 'http://api.flickr.com/services/feeds/photos_public.gne',
+   type: 'GET',
+   dataType: 'jsonp',
+   jsonp: 'jsoncallback', // a renseigner d'après la doc du service, par défaut callback
+   data:'tags=nantes&tagmode=any&format=json',
+   success: function (data) {
+     $.each(data.items, function(i,item){
+            $("<img/>").attr("src", item.media.m).appendTo("#panneau");
+            if ( i == 16 ) {
+              return false ; 
+            }});
+          },
+    error: function(resultat,statut,erreur){
+    alert("erreur");},
+});
+
   $( "#nomVille").autocomplete({
       source: "spip.php?page=autocomplete",
       minLength: 2,
