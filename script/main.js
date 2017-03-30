3b95b58effc9f01772acd148d16dcb9b61693579
Youlounn <yohann.rialet28@gmail.com>
2017-03-30 21:26:19 +0200

    Récup contenu champ text et envoi requête au clic
    Bug d'envoi, un peu aléatoirement rien n'est retourné

--- script/main.js
+++ script/main.js
@@ -1,85 +1,92 @@
-$(function(){
-  $(".ui-dialog").hide();
-  var dialog = false;
-  var bool = 0;
-  var nb = 20;
+$(function() {
+    $(".ui-dialog").hide();
+    var dialog = false;
+    var bool = 0;
+    var nb = 20;
 
-  $("#plus").click(function(){
-    if(bool==0){
-      $("#reglages").css("display","block");
-      bool = 1;
-    }
-    else{
-      $("#reglages").css("display","none");
-      bool = 0;
-    }
-  })
-  $("#incrementer").click(function(){
-    nb = $("#reglages input").val();
-    nb++;
-    $("#reglages input").val(nb);
-  })
-  $("#decrementer").click(function(){
-    nb = $("#reglages input").val();
-    nb--;
-    $("#reglages input").val(nb);
-  })
+    $("#plus").click(function() {
+        if (bool == 0) {
+            $("#reglages").css("display", "block");
+            bool = 1;
+        } else {
+            $("#reglages").css("display", "none");
+            bool = 0;
+        }
+    })
 
-  var listeVille = [];
-<<<<<<< HEAD
-  $("#tabs").tabs();
-  $("#dialog").dialog();
-=======
-  $( "#tabs" ).tabs();
-  $( "#dialog" ).dialog();
-   $('#tabPhoto').DataTable();
->>>>>>> origin/master
+    $("#incrementer").click(function() {
+        nb = $("#reglages input").val();
+        nb++;
+        $("#reglages input").val(nb);
+    })
+    $("#decrementer").click(function() {
+        nb = $("#reglages input").val();
+        nb--;
+        $("#reglages input").val(nb);
+    })
 
-  $.ajax({
-   url: 'http://api.flickr.com/services/feeds/photos_public.gne',
-   type: 'GET',
-   dataType: 'jsonp',
-   jsonp: 'jsoncallback', // a renseigner d'après la doc du service, par défaut callback
-   data:'tags=nantes&tagmode=any&format=json',
-   success: function (data) {
-     $.each(data.items, function(i,item){
-     var photo = $("<img/>").attr("src", item.media.m);
-     photo.attr("class", "itemPhoto").appendTo("#tabs-1");
+    var listeVille = [];
+    $("#tabs").tabs();
+    $("#dialog").dialog();
+    $("#tabs").tabs();
+    $("#dialog").dialog();
+    $('#tabPhoto').dataTable();
 
-     var src = item.media.m;
-     var auteur = item.author;
-     var titre = item.title;
-     var date = item.date;        
-     $(".ui-dialog").css("visibility","hidden");
-     $(".ui-dialog").css("position","absolute");
-     $(".ui-dialog").css("left","2%");
-     $(".ui-dialog").css("top","60%");
+    $("#loupe").click(function() {
+        var ville = $('#nomVille').val();
+        var textData = 'tags=ville'+ville+'&tagmode=any&format=json';
+        $.ajax({
+            url: 'http://api.flickr.com/services/feeds/photos_public.gne',
+            type: 'GET',
+            dataType: 'jsonp',
+            jsonp:   'jsoncallback',
+              // a renseigner d'après la doc du service, par défaut callback
+            data: textData,
+            success: function(data) {
+              alert("test1");
+              alert(JSON.stringify(data));
+                $.each(data.items,  function(i, item) {
+                    var photo = $("<img/>").attr("src",  item.media.m);
+                    photo.attr("class", "itemPhoto").appendTo("#tabs-1");
 
-     photo.click(function(){
-       if(dialog == false){
-         $('#dialog').append("<p>"+titre+"</br>"+auteur+"</p>")
-         $(".ui-dialog").css("visibility","visible");
-         dialog = true;
-       }
-       else{
-         $('#dialog').empty();
-         $(".ui-dialog").css("visibility","hidden");
-         dialog = false;
-       }
-     });
+                    var src = item.media.m;
+                    var auteur = item.author;
+                    var titre = item.title;
+                    var date = item.date;        
+                    $(".ui-dialog").css("visibility", "hidden");
+                    $(".ui-dialog").css("position", "absolute");
+                    $(".ui-dialog").css("left", "2%");
+                    $(".ui-dialog").css("top", "60%");
 
-            if ( i == $("#nombrePhoto").val) {
-              return false;
-            }});
-          },
-    error: function(resultat,statut,erreur){
-    alert("erreur");},
-});
+                    photo.click(function() {
+                        if (dialog == false) {
+                            $('#dialog').append("<p>" + titre + "</br>" + auteur + "</p>")
+                            $(".ui-dialog").css("visibility", "visible");
+                            dialog = true;
+                        } else {
+                            $('#dialog').empty();
+                            $(".ui-dialog").css("visibility", "hidden");
+                            dialog = false;
+                        }
+                    });
 
-  $( "#nomVille").autocomplete({
-      source: "spip.php?page=autocomplete",
-      minLength: 2,
+                    if  ( i  ==  $("#nombrePhoto").val)  {
+                        return  false;
+                    }
+                });          
+            },
+            error:   function(resultat, statut, erreur) {
+                alert("erreur");
+            },
+        });
     });
+
+
+    //
+    // $("#nomVille").autocomplete({
+    //     source: "spip.php?page=autocomplete",
+    //     minLength: 2,
+    // });
 
 
 });
