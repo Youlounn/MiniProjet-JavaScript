034406edf93d7ba420c7aed72e79bafb7346a7b7
Youlounn <yohann.rialet28@gmail.com>
2017-04-04 21:41:23 +0200

    Fin

--- script/main.js
+++ script/main.js
@@ -14,19 +14,19 @@ $(function() {
     })
 
     $("#incrementer").click(function() {
-        if($("#reglages input").val() == 20){
-          $("#reglages input").val(20);
+        if($("#nombrePhoto").val() == 20){
+          $("#nombrePhoto").val(20);
         }
         else{
-          nb = $("#reglages input").val();
+          nb = $("#nombrePhoto").val();
           nb++;
-          $("#reglages input").val(nb);
+          $("#nombrePhoto").val(nb);
         }
     })
     $("#decrementer").click(function() {
-        nb = $("#reglages input").val();
+        nb = $("#nombrePhoto").val();
         nb--;
-        $("#reglages input").val(nb);
+        $("#nombrePhoto").val(nb);
     })
 
     var listeVille = [];
@@ -36,16 +36,28 @@ $(function() {
     $("#dialog").dialog();
     $('#tabPhoto').dataTable();
 
+
     $(".ui-button").click(function() {
         $(".ui-dialog").css("display", "none");
         $('#dialog').empty();
         dialog = false;
     });
 
+    var tableBis = $('#tableau').DataTable({
+      "columns":[{
+        "type" : "string"
+      },{
+        "type" : "string"
+      },{
+        "type" : "string"
+      }],
+    });
+
     $("#loupe").click(function() {
         $("#soustabs1").empty();
         var ville = $('#nomVille').val();
         var textData = '&tags=' + ville + '&tagmode=all&format=json';
+
 
         $.ajax({
             url: 'http://api.flickr.com/services/feeds/photos_public.gne',
@@ -55,21 +67,68 @@ $(function() {
               // a renseigner d'après la doc du service, par défaut callback
             data: textData,
             success: function(data) {
-                alert(JSON.stringify(data));
+                tableBis.clear();
+                var nbDemande = $('#nombrePhoto').val();
+                var cpt = 0;
                 $.each(data.items,  function(i, item) {
+                  console.log("i = "+i);
+                    var dateMin = $('#dateMin').val();
+                    var date = item.date_taken.substring(0,10);
+
+                    var anneeDate = parseInt(date.substring(0,4));
+                    var anneeDateMin = parseInt(dateMin.substring(0,4));
+                    var moisDate = parseInt(date.substring(5,7));
+                    var moisDateMin = parseInt(dateMin.substring(5,7));
+                    var jourDate = parseInt(date.substring(8,10));
+                    var jourDateMin = parseInt(dateMin.substring(8,10));
+
                     var photo = $("<img/>").attr("src",  item.media.m);
-                    photo.attr("class", "itemPhoto").appendTo("#soustabs1");
                     var src = item.media.m;
-                    var auteur = item.author;
                     var titre = item.title;
-                    var date = item.date;        
+                    var auteur = item.author;
+                    var photo2 = "<img src="+src+" class='photos'></img>"
+
+                    var continuer = true;
+                    if(anneeDateMin < anneeDate){
+                      photo.attr("class", "itemPhoto").appendTo("#soustabs1");
+                      tableBis.row.add([
+                        photo2,
+                        auteur,
+                        date
+                      ]).draw(true);
+                      cpt++;
+                      continuer = false;
+                    }
+                    else if(anneeDateMin == anneeDate && continuer == true){
+                      if(moisDateMin < moisDate && continuer == true){
+                        photo.attr("class", "itemPhoto").appendTo("#soustabs1");
+                        tableBis.row.add([
+                          photo2,
+                          auteur,
+                          date
+                        ]).draw(true);
+                        cpt++;
+                        continuer = false;
+                      }
+                      else if(moisDateMin == moisDate && continuer == true)
+                        if(jourDateMin <= jourDate && continuer == true){
+                          photo.attr("class", "itemPhoto").appendTo("#soustabs1");
+                          tableBis.row.add([
+                            photo2,
+                            auteur,
+                            date
+                          ]).draw(true);
+                          cpt++;
+                          continuer = false;
+                        }
+                    }
+
                     $(".ui-dialog").css("visibility", "hidden");
                     $(".ui-dialog").css("position", "absolute");
                     $(".ui-dialog").css("left", "2%");
                     $(".ui-dialog").css("top", "60%");
 
                     photo.click(function() {
-                        console.log("Dialog : " + dialog);
                         if (dialog == false) {
                             $('#dialog').append("<p>" + titre + "</br>" + auteur + "</p>");
                             $(".ui-dialog").css("visibility", "visible");
@@ -82,13 +141,30 @@ $(function() {
                             dialog = false;
                         }
                     });
-                    var tmp = $("#reglages input").val();
-                    console.log("i = " + i);
-                    console.log("tmp = " + tmp);
-                    if  (i  ==  tmp - 1)  {
-                        return  false;
+                    if(nbDemande == cpt){
+                      return false;
                     }
-                    i = i - 1;
+                    if((i == 19 || i == nbDemande) && cpt == 0){
+                      alert("test");
+                      $('#dialog').empty();
+                      $('#dialog').append("<p>Désolé, nous n'avons pas de résultat.</p>");
+                      $(".ui-dialog").css("visibility", "visible");
+                      $(".ui-dialog").css("display", "block");
+                      tableBis.clear().draw(true);
+                      dialog = true;
+                      return false;
+                    }
+                    if(i == 19 && cpt < nbDemande){
+                      $('#dialog').empty();
+                      $('#dialog').append("<p>Désolé, nous n'avons pas assez de résultats.</p>");
+                      $(".ui-dialog").css("visibility", "visible");
+                      $(".ui-dialog").css("display", "block");
+                      dialog = true;
+                      return false;
+                    }
+                    alert('test avant');
+                    tableBis.draw(true);
+                    alert('test après');
                 });          
             },
             error:   function(resultat, statut, erreur) {
